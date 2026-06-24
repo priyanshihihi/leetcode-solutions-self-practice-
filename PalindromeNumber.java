9f991e33f83247e20e1f8503c972c09279d903d6
Your Name <your_email@example.com>
2026-06-24 23:25:19 +0530

    Solved LeetCode 9 Palindrome Number

---
+++ PalindromeNumber.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));   // true
+        System.out.println(isPalindrome(-121));  // false
+        System.out.println(isPalindrome(10));    // false
+    }
+}
