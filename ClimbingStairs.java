8198d37c5cc2c93c837425777f8706bf332a197d
Your Name <your_email@example.com>
2026-06-25 20:47:32 +0530

    Solved LeetCode 70 Climbing Stairs

---
+++ ClimbingStairs.java
@@ -0,0 +1,25 @@
+public class ClimbingStairs {
+
+    public static int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int first = 1;
+        int second = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return second;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(2));
+        System.out.println(climbStairs(3));
+        System.out.println(climbStairs(5));
+    }
+}
