1b89ae74437f011a7990e91c04bb2936af7af764
Priyanshi Trivedi <129305174+priyanshihihi@users.noreply.github.com>
2026-07-05 14:46:07 +0530

    Time: 14 ms (58.82%), Space: 47.4 MB (15.29%) - LeetHub

---
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] pathsWithMaxScore(List<String> board) {
+        int n = board.size();
+        int MOD = 1000000007;
+
+        int[][] score = new int[n][n];
+        int[][] ways = new int[n][n];
+
+        for (int i = 0; i < n; i++)
+            Arrays.fill(score[i], -1);
+
+        score[n - 1][n - 1] = 0;
+        ways[n - 1][n - 1] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (board.get(i).charAt(j) == 'X' || (i == n - 1 && j == n - 1))
+                    continue;
+
+                int best = -1, cnt = 0;
+
+                int[][] dir = {{1, 0}, {0, 1}, {1, 1}};
+
+                for (int[] d : dir) {
+                    int x = i + d[0], y = j + d[1];
+
+                    if (x < n && y < n && score[x][y] != -1) {
+                        if (score[x][y] > best) {
+                            best = score[x][y];
+                            cnt = ways[x][y];
+                        } else if (score[x][y] == best) {
+                            cnt = (cnt + ways[x][y]) % MOD;
+                        }
+                    }
+                }
+
+                if (best == -1) continue;
+
+                char c = board.get(i).charAt(j);
+                if (c != 'E')
+                    best += c - '0';
+
+                score[i][j] = best;
+                ways[i][j] = cnt;
+            }
+        }
+
+        if (ways[0][0] == 0) return new int[]{0, 0};
+        return new int[]{score[0][0], ways[0][0]};
+    }
+}
